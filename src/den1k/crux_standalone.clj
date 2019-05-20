e399454312729139d0bd50072a405ac5e0503eda
den1k <kalavox@gmail.com>
2019-05-20 10:52:07 -0400

    Fix wrong start function

--- src/den1k/crux_standalone.clj
+++ src/den1k/crux_standalone.clj
@@ -1,5 +1,5 @@
 (ns den1k.crux-standalone
-  (:require  [crux.api :as crux])
+  (:require [crux.api :as crux])
   (:import (crux.api ICruxAPI)))
 
 (def crux-options
@@ -7,17 +7,18 @@
    :db-dir     "data/db-dir-1"})
 
 (defn run-system [{:keys [server-port] :as options} with-system-fn]
-  (with-open [crux-system (crux/start-cluster-node options)]
+  (with-open [crux-system (crux/start-standalone-system options)]
     (with-system-fn crux-system)))
 
 (declare s crux)
 (comment
- (def s (future
-         (run-system
-          crux-options
-          (fn [crux-system]
-            (def crux crux-system)
-            (Thread/sleep Long/MAX_VALUE)))))
+ (def ^ICruxAPI s
+   (future
+    (run-system
+     crux-options
+     (fn [crux-system]
+       (def crux crux-system)
+       (Thread/sleep Long/MAX_VALUE)))))
  (future-cancel s)
 
  (.close system)
